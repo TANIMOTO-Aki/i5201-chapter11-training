2f9b28e0d901ec128fbcb458186cc558b6abd4f8
TANIMOTO-Aki <kisy23011@kibireha.ac.jp>
2024-05-09 14:52:45 +0900

    配列生成に関するプログラムとして、ファイルの要素数を数えるfileCountメソッド、数えた数を基に配列を生成するgenerateArrayメソッド、生成した配列に要素を格納するstoresElementsメソッドを作成しました。

--- src/main/java/EX11/EX11_MeiboSearch.java
+++ src/main/java/EX11/EX11_MeiboSearch.java
@@ -1,14 +1,26 @@
+import java.util.*;
+
 public class EX11_MeiboSearch {
-    int[] arrayNumber;
-    String[] arrayName;
-    String[] arrayBirth;
-    String[] arrayAddress;
+    static FileIn fi = new FileIn();
+    static String[] arrayNumber;
+    static String[] arraySurname;
+    static String[] arrayFirstname;
+    static String[] arrayBirthyear;
+    static String[] arrayBirthmonth;
+    static String[] arrayBirthday;
+    static String[] arrayAddress;
 
     public static void main(String[] args) {
         KeyIn ki = new KeyIn();
         String command;
+        String filename = args[0];
         String date = GetDate.getFormattedCurrentDate();
+        int count;
         boolean flag = false;
+
+        count = fileCount(filename);
+        generateArray(count);
+        storesElements(filename);
 
         System.out.println("<EX_MeiboSearch> **********　名簿照会処理　********** (" + date + "現在)"
                 + "\n" +"-----------------------------------------------------------"
@@ -41,5 +53,56 @@ public class EX11_MeiboSearch {
                     break;
             }
         } while(flag == false);
+    }
+
+    public static int fileCount(String filename){
+        String buff;
+        int count = 0;
+        fi.open(filename);
+        buff = fi.readLine();
+        while (buff != null) {
+            count++;
+            buff = fi.readLine();
+        }
+        fi.close();
+        return count;
+    }
+
+    public static void generateArray(int count){
+        arrayNumber = new String[count];
+        arraySurname = new String[count];
+        arrayFirstname = new String[count];
+        arrayBirthyear = new String[count];
+        arrayBirthmonth = new String[count];
+        arrayBirthday = new String[count];
+        arrayAddress = new String[count];
+        return;
+    }
+
+    public static void storesElements(String filename){
+        final int NUMBER_POSITION = 0;
+        final int SURNAME_POSITION = 1;
+        final int FIRSTNAME_POSITION = 2;
+        final int YEAR_POSITION = 4;
+        final int MONTH_POSITION = 5;
+        final int DAY_POSITION = 6;
+        final int ADDRESS_POSITION = 7;
+
+        fi.open(filename);
+        String buff = fi.readLine();
+
+        for (int i = 0; i < arrayNumber.length;i++){
+            String[] element = buff.split(",");
+            arrayNumber[i] = element[NUMBER_POSITION];
+            arraySurname[i] = element[SURNAME_POSITION];
+            arrayFirstname[i] = element[FIRSTNAME_POSITION];
+            arrayBirthyear[i] = element[YEAR_POSITION];
+            arrayBirthmonth[i] = element[MONTH_POSITION];
+            arrayBirthday[i] = element[DAY_POSITION];
+            arrayAddress[i] = element[ADDRESS_POSITION];
+            buff = fi.readLine();
+        }
+        fi.close();
+        return;
     }
 }
